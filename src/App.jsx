79095111023b7282f4e21377781a4b2bff2b3399
Nissan Barua <baruanissan55.com>
2024-11-05 22:32:52 +0600

    added header folder, blogjson file, blog folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Blog from "./Components/Blog/Blog";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+     <Header></Header>
+     <Blog></Blog>
     </>
   );
 }
